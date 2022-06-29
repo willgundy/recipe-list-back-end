a2be31c9087f0547d7a6bf3456e405526bcc04c3
Will Gundy <willgunderson503@gmail.com>
2022-06-29 15:09:06 -0700

    added test for post

--- __tests__/recipe.test.js
+++ __tests__/recipe.test.js
@@ -14,6 +14,13 @@ describe('backend-express-template routes', () => {
     expect(resp.body.length).toEqual(2);
   });
 
+  it('should create a new recipe when called', async () => {
+    const resp = await request(app).post('/api/v1/recipes').send({ title: 'test', description: 'test' });
+    expect(resp.status).toEqual(200);
+    const res = await request(app).get('/api/v1/recipes');
+    expect(res.body.length).toEqual(3);
+  });
+
   afterAll(() => {
     pool.end();
   });
