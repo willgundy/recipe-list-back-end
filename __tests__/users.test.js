c8867d74cbee7e91ee97fe8d36e6eb6d20f629dd
Will Gundy <willgunderson503@gmail.com>
2022-06-29 16:22:30 -0700

    add test file for users

---
+++ __tests__/users.test.js
@@ -0,0 +1,45 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  email: 'test@test.com',
+  password: '123456',
+};
+
+const createAndLogin = async (userProps = {}) => {
+  const agent = request.agent(app);
+  // const user = await 
+};
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('should create a new user', async () => {
+    const resp = await request(app).post('/api/v1/users').send(mockUser);
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      email: mockUser.email,
+    });
+  });
+
+  it.skip('returns the current logged in user', async () => {
+
+  });
+
+  it.skip('should delete the session of the user', async () => {
+
+  });
+
+  it.skip('should sign in the user', async () => {
+
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS recipes;
+
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR NOT NULL UNIQUE,
+  password_hash VARCHAR NOT NULL
+);
 
 CREATE TABLE recipes (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
