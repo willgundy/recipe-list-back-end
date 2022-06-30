2b5f4e7e7b4d3a2a60315ef31270983df6b2e0d2
Cody-Wise <cody@sharptackmedia.com>
2022-06-30 15:06:24 -0700

    Update test is passing

--- __tests__/recipe.test.js
+++ __tests__/recipe.test.js
@@ -15,7 +15,9 @@ describe('backend-express-template routes', () => {
   });
 
   it('should create a new recipe when called', async () => {
-    const resp = await request(app).post('/api/v1/recipes').send({ title: 'test', description: 'test' });
+    const resp = await request(app)
+      .post('/api/v1/recipes')
+      .send({ title: 'test', description: 'test' });
     expect(resp.status).toEqual(200);
     const res = await request(app).get('/api/v1/recipes');
     expect(res.body.length).toEqual(3);
@@ -33,6 +35,17 @@ describe('backend-express-template routes', () => {
     expect(resp.body.id).toEqual('1');
     const res = await request(app).get('/api/v1/recipes');
     expect(res.body.length).toEqual(1);
+  });
+
+  it('update recipe should update a particular recipe', async () => {
+    const resp = await request(app)
+      .put('/api/v1/recipes/2')
+      .send({ title: 'title', description: 'description' });
+    console.log(resp.body);
+    expect(resp.status).toEqual(200);
+    expect(resp.body.id).toEqual('2');
+    const res = await request(app).get('/api/v1/recipes');
+    expect(res.body.length).toEqual(2);
   });
 
   afterAll(() => {
